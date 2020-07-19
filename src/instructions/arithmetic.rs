c8db135a8f3e70103d4562900a6238b217e6941f
Denis Vasilik <contact@denisvasilik.com>
2020-07-19 15:40:59 +0200

    Use llvm_asm! instead of asm!

--- src/instructions/arithmetic.rs
+++ src/instructions/arithmetic.rs
@@ -3,7 +3,7 @@
 pub fn add(a: i32, b: i32) -> i32 {
     let c: i32;
     unsafe {
-        asm!("add $0, $1, $2" : "=r"(c): "r"(a), "r"(b) :: "volatile")
+        llvm_asm!("add $0, $1, $2" : "=r"(c): "r"(a), "r"(b) :: "volatile")
     }
     c
 }
--- src/instructions/misc.rs
+++ src/instructions/misc.rs
@@ -2,14 +2,14 @@
 /// A no-operation. Useful to prevent delay loops from being optimized away.
 #[inline]
 pub fn nop() {
-    unsafe { 
-        asm!("nop" :::: "volatile") 
+    unsafe {
+        llvm_asm!("nop" :::: "volatile")
     }
 }
 
 #[inline]
 pub unsafe fn svc(id: usize, nr: usize, arg: usize) -> usize {
     let mut asm_nr = nr;
-    asm!("svc 0x00123456" : "+{r0}"(asm_nr) : "{r1}"(arg) :: "volatile");
+    llvm_asm!("svc 0x00123456" : "+{r0}"(asm_nr) : "{r1}"(arg) :: "volatile");
     asm_nr
 }
